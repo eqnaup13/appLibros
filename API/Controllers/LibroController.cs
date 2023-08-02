66ae014a83eb78b34c4882ab2e18cc8e88cd06c5
eqnaup13 <erodriguez.system@gmail.com>
2023-08-02 14:27:36 -0500

    Agregar archivos de proyecto.

---
+++ API/Controllers/LibroController.cs
@@ -0,0 +1,27 @@
+﻿using Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models.Entidades;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibroController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LibroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Libro>>> Get()
+        {
+            var lista = await _context.Libros.Include(c=>c.Categoria).ToListAsync();
+            return Ok(lista);
+        }
+    }
+}
